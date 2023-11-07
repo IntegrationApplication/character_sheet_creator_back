869ebedf6b76e49ce0921eab078a0b2f445e26b0
L0lingJack <odier.kelvin@gmail.com>
2023-11-07 16:01:17 +0100

    added the base of Character class

---
+++ CharacterSheetCreatorBack/CharacterSheetCreatorBack/Character.cs
@@ -0,0 +1,25 @@
+﻿namespace CharacterSheetCreatorBack
+{
+    public class Character
+    {  
+        public String Name {get;set;}
+        public String PlayerID { get;set;}
+        public String PlayerName { get;set;}    
+        public String Background { get;set;}
+        public String Alignement { get;set;}
+        public String Race { get;set;}
+        public String ClassAndLEvel { get;set;}
+
+
+        public Character()
+        {
+            Name = "";
+            PlayerID = "";
+            PlayerName = "";
+            Background = "";
+            Alignement = "";
+            Race = "";
+            ClassAndLEvel = "";
+        }
+    }
+}

a227f0ec177d60c04b137a031a82050b9b649bc4
rechassagn1 <rechassagn1@a208pc08L.local.isima.fr>
2023-12-06 10:56:50 +0100

    simplify attack class

--- CharacterSheetCreatorBack/CharacterSheetCreatorBack/Classes/Attack.cs
+++ CharacterSheetCreatorBack/CharacterSheetCreatorBack/Classes/Attack.cs
@@ -7,25 +7,9 @@ namespace CharacterSheetCreatorBack.Classes
         [Key]
         public int Id { get; set; }
         public Ability LinkedAbility { get; set; }
-        public string DamageType { get; set; }
-        public int[] DamageDice { get; set; }
-
-        public Attack() {
-            LinkedAbility = new Ability();
-            DamageType = string.Empty;
-            DamageDice = new int[3];
-        }
-
-        public int Roll()
-        {
-            int total = 0;
-            var rand = new System.Random();
-
-            for (int i = 0; i < DamageDice[0]; ++i)
-            {
-                total += rand.Next() % DamageDice[1] + 1;
-            }
-            return total + DamageDice[2];
-        }
+        public string DamageType { get; set; } = string.Empty;
+        public int NbDices { get; set; } = default(int);
+        public int DicesFaces { get; set; } = default(int);
+        public int DamageBonus { get; set; } = default(int);
     }
 }

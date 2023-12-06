dc6e5abe91b931bb278a55e6fc1aa219af6d0216
rechassagn1 <rechassagn1@a208pc08L.local.isima.fr>
2023-12-06 10:56:35 +0100

    ability is now an enum

--- CharacterSheetCreatorBack/CharacterSheetCreatorBack/Classes/Ability.cs
+++ CharacterSheetCreatorBack/CharacterSheetCreatorBack/Classes/Ability.cs
@@ -2,31 +2,31 @@
 
 namespace CharacterSheetCreatorBack.Classes
 {
-    public class Ability
+    public enum Ability
     {
-        [Key]
-        public String Name { get; set; }
-        public int Value { get; set; }
-        public int Modifier { get; set; }
-
-        public Ability()
-        {
-            Name = "";
-            Value = 0;
-            Modifier = 0;
-        }
-
-        public Ability(String name, int value, int modifier)
-        {
-            Name = name;
-            Value = value;
-            Modifier = modifier;
-        }
-
-        public int Roll()
-        {
-            var rand = new System.Random();
-            return rand.Next() % 20 + 1 + Modifier;
-        }
+        Strenght,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma,
+        Acrobatics,
+        Animal_handling,
+        Arcana,
+        Athletics,
+        Deception,
+        History,
+        Insight,
+        Intimidation,
+        Investigation,
+        Medicine,
+        Nature,
+        Perception,
+        Performance,
+        Persuasion,
+        Religion,
+        Sleight_of_hand,
+        Stealth,
+        Survival
     }
 }

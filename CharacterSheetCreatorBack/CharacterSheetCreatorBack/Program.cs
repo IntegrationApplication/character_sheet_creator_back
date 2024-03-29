815725a30d6accc2b3d168f8ff6d0b20aae435c6
drfailer <drfailer.42@gmail.com>
2024-03-29 20:23:15 +0100

    fix swagger on linux + connection string for docker

--- CharacterSheetCreatorBack/CharacterSheetCreatorBack/Program.cs
+++ CharacterSheetCreatorBack/CharacterSheetCreatorBack/Program.cs
@@ -19,7 +19,10 @@ builder.Services.AddCors(options => {
 });
 
 // Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true
-builder.Services.AddDbContext<RpgContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true"));
+/* builder.Services.AddDbContext<RpgContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true")); */
+builder.Services.AddDbContext<RpgContext>(options =>
+        options.UseSqlServer("Server=sql_server2022;Database=JDRDB;User Id=SA;Password=S3cur3P@ssW0rd!;MultipleActiveResultSets=true")
+);
 
 var app = builder.Build();
 
@@ -27,7 +30,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
